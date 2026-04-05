3573a655ee0fa0f1f68934ec06c12a4bfcb79ac3
golcmit <berutoma@gmail.com>
2026-04-05 14:51:53 +0900

    for 1.20.4 add

---
+++ common/src/main/java/com/golcmit/alodestar/Constants.java
@@ -0,0 +1,11 @@
+package com.golcmit.alodestar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "alodestar";
+	public static final String MOD_NAME = "Alodestar";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
